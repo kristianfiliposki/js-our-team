ce34132c9ec0794d6152ab5848f81e9da80b4db7
kristiafiliposki <filiposki.kristian1112@gmail.com>
2023-11-13 10:56:09 +0100

    1.1

--- java/main.js
+++ java/main.js
@@ -2,36 +2,49 @@ const utenti=[
     {
         nome:"Wayne ",
         cognome:"Barnett",
-        professione:"Founder & CEO",    
+        professione:"Founder & CEO", 
+        immagine:"./img 4/angela-caroll-chief-editor.jpg"   
     },
     {
         nome:"Angela",
         cognome:"Caroll",
         professione:"Chief Editor",
+        immagine:"./img 4/angela-caroll-chief-editor.jpg"
     },
     {
         nome:"Walter",
         cognome:"Gordon",
         professione:"Office Manager",
+        immagine:"./img 4/walter-gordon-office-manager.jpg"
     },
     {
         nome:"Angela ",
         cognome:"Lopez",
-        professione:"Social Media Manager"
+        professione:"Social Media Manager",
+        immagine:"./img 4/angela-lopez-social-media-manager.jpg"
     },
     {
         nome:"Scott ",
         cognome:"Estrada",
-        professione:"Developer"
+        professione:"Developer",
+        immagine:"./img 4/scott-estrada-developer.jpg"
     },
     {
         nome:"Barbara Ramos ",
         cognome:"Ramos",
         professione:"Graphic Designer",
+        immagine:"./img 4/barbara-ramos-graphic-designer.jpg"
+    
     }];
     
     
     
+    /* 
+    let img2.src = "./img 4/angela-caroll-chief-editor.jpg";
+    let img3.src = "./img 4/walter-gordon-office-manager.jpg";
+    let img4.src = "./img 4/angela-lopez-social-media-manager.jpg";
+    let img5.src = "./img 4/scott-estrada-developer.jpg";
+    let img6.src = "./img 4/barbara-ramos-graphic-designer.jpg"; */
     
     
     
@@ -42,22 +55,6 @@ const utenti=[
     /* fai un ciclo */
     /* l'elemento del ciclo sara il n.delle foto */
     /* poni che aumentando il n. delle foto,aumenti nel medesimo modo anche il n. che rappresenta l'utente. */
-    
-    /* function GenCard (immagine){
-        immagine=document.createElement("img");
-    immagine.classList.add("view");
-    images.push(immagine);
-    container.append(immagine);
-    immagine.src = "./img 4/wayne-barnett-founder-ceo.jpg";
-} */
-
-/* 
-let img2.src = "./img 4/angela-caroll-chief-editor.jpg";
-let img3.src = "./img 4/walter-gordon-office-manager.jpg";
-let img4.src = "./img 4/angela-lopez-social-media-manager.jpg";
-let img5.src = "./img 4/scott-estrada-developer.jpg";
-let img6.src = "./img 4/barbara-ramos-graphic-designer.jpg"; */
-
 
 
 const container=document.getElementById("container");
@@ -66,14 +63,16 @@ for (let i = 0; i < utenti.length; i++) {
     const utente = utenti[i];
     const Fotdiv=document.createElement("div");
     container.append(Fotdiv)
-    container.innerHTML+=
     
-    (   
-        `il suo nome è:${utente.nome},
-        il suo nome è:${utente.cognome},
-        la sua professione è:${utente.professione}`
-        )
-        
+    container.innerHTML+=
+`<div>
+    <img src=${utente.immagine}>
+    <div>il suo nome è:${utente.nome}</div>
+    <div>il suo cognome è:${utente.cognome},</div>
+    <div>la sua professione è:${utente.professione}</div>
+</div>`;
+
     }
+    
     
     
